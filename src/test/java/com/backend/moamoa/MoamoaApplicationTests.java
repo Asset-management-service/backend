2df54d1e8e12f7913b1426e70844e11590971882
ehgns852 <ehgns5668@naver.com>
2022-03-12 11:49:16 +0900

    first commit

---
+++ src/test/java/com/backend/moamoa/MoamoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.moamoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoamoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
