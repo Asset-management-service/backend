b8bb35c694b496407464b3643ec10bcebea4068b
이승수 <66988341+sseung12@users.noreply.github.com>
2022-03-20 23:21:26 +0900

    [Refactor] camelcase 변경
    기존 _ 구조에서 camelcase로 변경했습니다.

--- src/main/java/com/backend/moamoa/domain/crawling/Stock.java
+++ src/main/java/com/backend/moamoa/domain/crawling/Stock.java
@@ -24,22 +24,22 @@ public class Stock {
     private String price;
 
     //시가
-    private String close_Price;
+    private String closePrice;
 
     //등락률
     private String rate;
 
     //고가
-    private String high_Price;
+    private String highPrice;
 
     //저가
-    private String low_Price;
+    private String lowPrice;
 
     //거래량(천주)
     private String volume;
 
 
     //전일종가
-    private String net_Change;
+    private String netChange;
 }
 
