665dccb135d87b72c486936c70f379b3a446dc15
ehgns852 <ehgns5668@naver.com>
2022-03-30 19:47:51 +0900

    [Add] 불필요한 파일 삭제 및 월급 관리 로직 생성

--- src/main/java/com/backend/moamoa/MoamoaApplication.java
+++ src/main/java/com/backend/moamoa/MoamoaApplication.java
@@ -1,16 +1,9 @@
 package com.backend.moamoa;
 
-import com.backend.moamoa.global.config.properties.AppProperties;
-import com.backend.moamoa.global.config.properties.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-		CorsProperties.class,
-		AppProperties.class
-})
 public class MoamoaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/backend/moamoa/domain/asset/salary/entity/Salary.java
+++ src/main/java/com/backend/moamoa/domain/asset/salary/entity/Salary.java
@@ -9,9 +9,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import static javax.persistence.CascadeType.ALL;
 import static javax.persistence.GenerationType.IDENTITY;
