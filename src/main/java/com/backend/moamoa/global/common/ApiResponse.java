76976cd7379017857d1dd9239fe9d4bcbca458c6
minu kim <kmw106933@naver.com>
2022-03-31 15:27:37 +0900

    [Feat] Api 응답을 위한 ApiResponse

--- src/main/java/com/backend/moamoa/global/common/ApiResponse.java
+++ src/main/java/com/backend/moamoa/global/common/ApiResponse.java
@@ -13,6 +13,7 @@ public class ApiResponse<T> {
     private final static int SUCCESS = 200;
     private final static int NOT_FOUND = 400;
     private final static int FAILED = 500;
+
     private final static String SUCCESS_MESSAGE = "SUCCESS";
     private final static String NOT_FOUND_MESSAGE = "NOT FOUND";
     private final static String FAILED_MESSAGE = "서버에서 오류가 발생하였습니다.";
