3b01b4d3b60cdf6f688cbc3a0378518e305caef8
minu kim <kmw106933@naver.com>
2022-04-03 03:02:33 +0900

    [Refactor] 필요없는 import 문 제거

--- src/main/java/com/backend/moamoa/global/config/AppConfig.java
+++ src/main/java/com/backend/moamoa/global/config/AppConfig.java
@@ -1,7 +1,5 @@
 package com.backend.moamoa.global.config;
 
-import com.backend.moamoa.domain.user.oauth.properties.CorsProperties;
-import com.backend.moamoa.global.config.AppProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
