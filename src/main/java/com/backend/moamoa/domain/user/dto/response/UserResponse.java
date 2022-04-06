7f47e5f4aa85411ff13a0a1b9cedee35f2910c98
minu kim <kmw106933@naver.com>
2022-04-07 03:11:15 +0900

    [Feat] 스웨거 설명 추가

--- src/main/java/com/backend/moamoa/domain/user/dto/response/UserResponse.java
+++ src/main/java/com/backend/moamoa/domain/user/dto/response/UserResponse.java
@@ -2,6 +2,9 @@ package com.backend.moamoa.domain.user.dto.response;
 
 import com.backend.moamoa.domain.user.entity.User;
 import com.backend.moamoa.domain.user.enums.Gender;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,24 +16,35 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ApiModel("유저 조회 정보")
+@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
 public class UserResponse {
 
+    @ApiModelProperty(value = "유저 식별자", example = "1")
     private Long id;
 
+    @ApiModelProperty(value = "유저 닉네임", example = "모아모아")
     private String nickname;
 
+    @ApiModelProperty(value = "유저 이메일", example = "kmw106933@naver.com")
     private String email;
 
+    @ApiModelProperty(value = "유저 전화번호", example = "01055555555")
     private String phoneNum;
 
+    @ApiModelProperty(value = "유저 성별", example = "MAN")
     private Gender gender;
 
+    @ApiModelProperty(value = "유저 생일", example = "03-20")
     private String birthday;
 
+    @ApiModelProperty(value = "유저 탄생년도", example = "2001")
     private String birthYear;
 
+    @ApiModelProperty(value = "생성 시각", example = "")
     private LocalDateTime createdDate;
 
+    @ApiModelProperty(value = "수정 시각", example = "")
     private LocalDateTime updatedDate;
 
     public static UserResponse toUserResponse(User user) {
