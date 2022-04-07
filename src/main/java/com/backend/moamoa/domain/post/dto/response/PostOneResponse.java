b62d2cc130f1e6761c4579df39239f3ec993b74b
ehgns852 <ehgns5668@naver.com>
2022-04-07 22:55:08 +0900

    [Add] Swagger API 명세서 작성(게시글, 댓글)

--- src/main/java/com/backend/moamoa/domain/post/dto/response/PostOneResponse.java
+++ src/main/java/com/backend/moamoa/domain/post/dto/response/PostOneResponse.java
@@ -2,6 +2,8 @@ package com.backend.moamoa.domain.post.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.querydsl.core.annotations.QueryProjection;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,31 +12,43 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+
+@ApiModel(description = "결과 응답 데이터 모델")
 @Getter
 @Setter
 @NoArgsConstructor
 public class PostOneResponse {
 
+    @ApiModelProperty(value = "게시글 Id")
     private Long postId;
 
+    @ApiModelProperty(value = "게시글 제목")
     private String title;
 
+    @ApiModelProperty(value = "게시글 내용")
     private String content;
 
+    @ApiModelProperty(value = "해당 게시글의 전체 스크랩 수")
     private int scrapCount;
 
+    @ApiModelProperty(value = "해당 게시글의 전체 댓글의 수")
     private int commentCount;
 
+    @ApiModelProperty(value = "해당 게시글의 전체 좋아요 수")
     private int likeCount;
 
+    @ApiModelProperty(value = "해당 게시글의 생성 시간")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createDate;
 
+    @ApiModelProperty(value = "해당 게시글의 수정 시간")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime updateDate;
 
+    @ApiModelProperty(value = "해당 게시글의 조회수")
     private Integer viewCount;
 
+    @ApiModelProperty(value = "해당 게시글의 생성 회원 이름")
     private String username;
 
     private List<PostOneCommentResponse> comments = new ArrayList<>();
--- src/main/java/com/backend/moamoa/domain/post/dto/response/RecentPostResponse.java
+++ src/main/java/com/backend/moamoa/domain/post/dto/response/RecentPostResponse.java
@@ -2,32 +2,43 @@ package com.backend.moamoa.domain.post.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.querydsl.core.annotations.QueryProjection;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@ApiModel(description = "결과 응답 데이터 모델")
 @Getter
 @Setter
 @NoArgsConstructor
 public class RecentPostResponse {
 
+    @ApiModelProperty(value = "게시글 Id")
     private Long postId;
 
+    @ApiModelProperty(value = "카테고리 이름")
     private String categoryName;
 
+    @ApiModelProperty(value = "게시글 제목")
     private String title;
 
+    @ApiModelProperty(value = "게시글 내용")
     private String content;
 
+    @ApiModelProperty(value = "해당 게시글의 댓글의 수")
     private int commentCount;
 
+    @ApiModelProperty(value = "게시글을 작성한 회원 이름")
     private String username;
 
+    @ApiModelProperty(value = "게시글 생성 시간")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime localDateTime;
 
+    @ApiModelProperty(value = "해당 게시글의 조회수")
     private int viewCount;
 
     @QueryProjection
