4046100b7d689b59e45dbb270fb08e1cfaa1b2d2
minu kim <kmw106933@naver.com>
2022-04-13 18:14:31 +0900

    [Refactor] 토큰 재발급 API 리팩터링

--- src/main/java/com/backend/moamoa/domain/user/oauth/controller/AuthController.java
+++ src/main/java/com/backend/moamoa/domain/user/oauth/controller/AuthController.java
@@ -1,101 +1,30 @@
 package com.backend.moamoa.domain.user.oauth.controller;
 
-import com.backend.moamoa.domain.user.entity.UserRefreshToken;
-import com.backend.moamoa.domain.user.enums.RoleType;
-import com.backend.moamoa.domain.user.oauth.filter.JwtFilter;
-import com.backend.moamoa.domain.user.oauth.provider.AuthToken;
-import com.backend.moamoa.domain.user.oauth.provider.AuthTokenProvider;
-import com.backend.moamoa.domain.user.repository.UserRefreshTokenRepository;
-import com.backend.moamoa.global.common.ApiResponse;
-import com.backend.moamoa.global.config.AppProperties;
-import com.backend.moamoa.global.utils.CookieUtil;
-import io.jsonwebtoken.Claims;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.backend.moamoa.domain.user.oauth.dto.request.TokenRequest;
+import com.backend.moamoa.domain.user.oauth.dto.response.TokenResponse;
+import com.backend.moamoa.domain.user.oauth.service.AuthService;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Api(tags = "토큰 재발급 API")
 public class AuthController {
 
-    private final AppProperties appProperties;
-    private final AuthTokenProvider tokenProvider;
-    private final AuthenticationManager authenticationManager;
-    private final UserRefreshTokenRepository userRefreshTokenRepository;
+    private final AuthService authService;
 
-    private final static long THREE_DAYS_MSEC = 259200000;
-    private final static String REFRESH_TOKEN = "refresh_token";
-
-    @GetMapping("/refresh")
-    public ApiResponse refreshToken (HttpServletRequest request, HttpServletResponse response) {
-        // access token 확인
-        String accessToken = JwtFilter.getAccessToken(request);
-        AuthToken authToken = tokenProvider.convertAuthToken(accessToken);
-        if (!authToken.validate()) {
-            return ApiResponse.invalidAccessToken();
-        }
-
-        // expired access token 인지 확인
-        Claims claims = authToken.getExpiredTokenClaims();
-        if (claims == null) {
-            return ApiResponse.notExpiredTokenYet();
-        }
-
-        String userId = claims.getSubject();
-        RoleType roleType = RoleType.of(claims.get("role", String.class));
-
-        // refresh token
-        String refreshToken = CookieUtil.getCookie(request, REFRESH_TOKEN)
-                .map(Cookie::getValue)
-                .orElse((null));
-        AuthToken authRefreshToken = tokenProvider.convertAuthToken(refreshToken);
-
-        if (authRefreshToken.validate()) {
-            return ApiResponse.invalidRefreshToken();
-        }
-
-        // userId refresh token 으로 DB 확인
-        UserRefreshToken userRefreshToken = userRefreshTokenRepository.findByUserIdAndRefreshToken(userId, refreshToken);
-        if (userRefreshToken == null) {
-            return ApiResponse.invalidRefreshToken();
-        }
-
-        Date now = new Date();
-        AuthToken newAccessToken = tokenProvider.createAuthToken(
-                userId,
-                roleType.getCode(),
-                new Date(now.getTime() + appProperties.getAuth().getTokenExpiry())
-        );
-
-        long validTime = authRefreshToken.getTokenClaims().getExpiration().getTime() - now.getTime();
-
-        // refresh 토큰 기간이 3일 이하로 남은 경우, refresh 토큰 갱신
-        if (validTime <= THREE_DAYS_MSEC) {
-            // refresh 토큰 설정
-            long refreshTokenExpiry = appProperties.getAuth().getRefreshTokenExpiry();
-
-            authRefreshToken = tokenProvider.createAuthToken(
-                    appProperties.getAuth().getTokenSecret(),
-                    new Date(now.getTime() + refreshTokenExpiry)
-            );
-
-            // DB에 refresh 토큰 업데이트
-            userRefreshToken.setRefreshToken(authRefreshToken.getToken());
-
-            int cookieMaxAge = (int) refreshTokenExpiry / 60;
-            CookieUtil.deleteCookie(request, response, REFRESH_TOKEN);
-            CookieUtil.addCookie(response, REFRESH_TOKEN, authRefreshToken.getToken(), cookieMaxAge);
-        }
-
-        return ApiResponse.success("token", newAccessToken.getToken());
+    @ApiOperation(value = "엑세스, 리프레시 토큰 재발급",
+            notes = "엑세스 토큰 만료시 회원 검증 후, 리프레시 토큰을 검증하여 엑세스 토큰과 리프레시 토큰을 재발급 합니다.")
+    @PostMapping("/reissue")
+    public TokenResponse reissue(
+            @ApiParam(value = "토큰 재발급 요청 데이터", required = true)
+            @RequestBody TokenRequest tokenRequest) {
+        return authService.reissue(tokenRequest);
     }
 
 }
