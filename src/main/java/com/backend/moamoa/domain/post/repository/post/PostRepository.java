e5c49f212f13303e65e3447269c10e38962a5ff4
minu kim <kmw106933@naver.com>
2022-04-13 20:22:41 +0900

    [Feat] PostID 내림차순(최신순)으로 데이터 가져오기 쿼리 추가

--- src/main/java/com/backend/moamoa/domain/post/repository/post/PostRepository.java
+++ src/main/java/com/backend/moamoa/domain/post/repository/post/PostRepository.java
@@ -1,7 +1,15 @@
 package com.backend.moamoa.domain.post.repository.post;
 
 import com.backend.moamoa.domain.post.entity.Post;
+import com.backend.moamoa.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
+    // PostId 내림차순으로 데이터 불러오기
+    Page<Post> findByUserOrderByIdDesc(User user, Pageable pageable);
+
 }
