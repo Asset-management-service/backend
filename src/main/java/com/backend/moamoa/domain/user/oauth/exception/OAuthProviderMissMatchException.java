1d25aed1fc4b7b3423c60f90b2144806014191ea
minu kim <kmw106933@naver.com>
2022-04-15 01:35:45 +0900

    [Refactor] 스프링 시큐리티 일부 파일 삭제

--- src/main/java/com/backend/moamoa/domain/user/oauth/exception/OAuthProviderMissMatchException.java
+++
@@ -1,8 +0,0 @@
-package com.backend.moamoa.domain.user.oauth.exception;
-
-public class OAuthProviderMissMatchException extends RuntimeException {
-
-    public OAuthProviderMissMatchException(String message) {
-        super(message);
-    }
-}
