dcdc2a9371222a7e78e8348f27e352b51b84728c
minu kim <kmw106933@naver.com>
2022-04-16 21:56:46 +0900

    [Refactor] refreshToken을 쿠키로 반환하도록 리팩터링

--- src/main/java/com/backend/moamoa/domain/user/service/AuthService.java
+++ src/main/java/com/backend/moamoa/domain/user/service/AuthService.java
@@ -1,17 +1,22 @@
 package com.backend.moamoa.domain.user.service;
 
 import com.backend.moamoa.domain.user.dto.request.LogoutRequest;
-import com.backend.moamoa.domain.user.dto.request.TokenRequest;
+import com.backend.moamoa.domain.user.dto.response.ReissueResponse;
 import com.backend.moamoa.domain.user.dto.response.TokenResponse;
+import com.backend.moamoa.domain.user.oauth.filter.JwtFilter;
 import com.backend.moamoa.domain.user.oauth.token.JwtProvider;
 import com.backend.moamoa.global.exception.CustomException;
 import com.backend.moamoa.global.exception.ErrorCode;
+import com.backend.moamoa.global.utils.CookieUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -20,45 +25,63 @@ import java.util.concurrent.TimeUnit;
 public class AuthService {
 
     private final JwtProvider jwtProvider;
+    private final JwtFilter jwtFilter;
     private final RedisTemplate redisTemplate;
-    private final long refreshTokenExpiry = 604800000;
 
+    private final static String REFRESH_TOKEN = "refresh_token";
+
+    /**
+     * 헤더에 담긴 AccessToken과 쿠키로 담은 RefreshToken을 검증하여
+     * AccessToken과 RefreshToken을 재발급 합니다.
+     *
+     * @return TokenResponse 토큰 반환 정보
+     */
     @Transactional
-    public TokenResponse reissue(TokenRequest tokenRequest) {
-        String accessToken = tokenRequest.getAccessToken();
+    public ReissueResponse reissue(HttpServletRequest request, HttpServletResponse response) {
+        String accessToken = jwtFilter.getAccessToken(request);
 
-        if (!jwtProvider.getExpiredTokenClaims(accessToken)) {
+        if (!jwtProvider.ExpiredToken(accessToken)) {
             throw new CustomException(ErrorCode.INVALID_TOKEN);
         }
 
-        String userId = jwtProvider.getUserInfo(accessToken).getSubject();
-        String refreshToken = (String) redisTemplate.opsForValue().get("RT:" + userId);
+        String userId = jwtProvider.parseClaims(accessToken).getSubject();
+        // redis로 부터 refreshToken 가져오기
+        String redisToken = (String) redisTemplate.opsForValue().get("RT:" + userId);
 
-        if (ObjectUtils.isEmpty(refreshToken) || !refreshToken.equals(tokenRequest.getRefreshToken())) {
+        // 쿠키로 부터 refreshToken 가져오기
+        String refreshToken = CookieUtil.getCookie(request, REFRESH_TOKEN)
+                .map(Cookie::getValue)
+                .orElse((null));
+
+        if (ObjectUtils.isEmpty(redisToken) || !redisToken.equals(refreshToken)) {
             throw new CustomException(ErrorCode.INVALID_REFRESH_TOKEN);
         }
 
         TokenResponse tokenResponse = jwtProvider.createTokenResponse(userId);
-        redisTemplate.opsForValue().set("RT:" + userId, tokenResponse.getRefreshToken(), refreshTokenExpiry, TimeUnit.MILLISECONDS);
+        redisTemplate.opsForValue().set("RT:" + userId, tokenResponse.getRefreshToken(), tokenResponse.getRefreshTokenExpireDate(), TimeUnit.MILLISECONDS);
 
-        return tokenResponse;
+        int cookieMaxAge = (int) tokenResponse.getRefreshTokenExpireDate() / 60;
+        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN);
+        CookieUtil.addCookie(response, REFRESH_TOKEN, tokenResponse.getRefreshToken(), cookieMaxAge);
+
+        return ReissueResponse.toReissueResponse(tokenResponse);
     }
 
-    public Boolean logout(LogoutRequest logoutRequest) {
-        String accessToken = logoutRequest.getAccessToken();
+    public Boolean logout(HttpServletRequest request) {
+        String accessToken = jwtFilter.getAccessToken(request);
 
-        if (!jwtProvider.getTokenClaims(accessToken)) {
+        if (!jwtProvider.validationToken(accessToken)) {
             throw new CustomException(ErrorCode.INVALID_TOKEN);
         }
 
-        String userId = jwtProvider.getUserInfo(accessToken).getSubject();
+        String userId = jwtProvider.parseClaims(accessToken).getSubject();
 
         if (redisTemplate.opsForValue().get("RT:" + userId) != null) {
             redisTemplate.delete("RT:" + userId);
         }
 
-        Long expiration = jwtProvider.getExpiration(logoutRequest.getAccessToken());
-        redisTemplate.opsForValue().set(logoutRequest.getAccessToken(), "logout", expiration, TimeUnit.MILLISECONDS);
+        Long expiration = jwtProvider.getExpiration(accessToken);
+        redisTemplate.opsForValue().set(accessToken, "logout", expiration, TimeUnit.MILLISECONDS);
 
         return true;
     }
