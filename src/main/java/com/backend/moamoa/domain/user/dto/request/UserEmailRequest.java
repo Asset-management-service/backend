6dee4fc13e3752048978803ade669324d52af520
minu kim <kmw106933@naver.com>
2022-04-16 21:58:34 +0900

    [Refactor] 유저 API 리팩터링

--- src/main/java/com/backend/moamoa/domain/user/dto/request/UserEmailRequest.java
+++ src/main/java/com/backend/moamoa/domain/user/dto/request/UserEmailRequest.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
@@ -16,8 +18,9 @@ import javax.validation.constraints.NotNull;
 @ApiModel("이메일 인증 요청 정보")
 public class UserEmailRequest {
 
-    @NotNull
+    @NotBlank(message = "이메일을 입력해주세요.")
     @ApiModelProperty(value = "인증할 이메일", required = true, example = "kmw106933@naver.com")
+    @Pattern(regexp = "^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\\.[A-Za-z0-9\\-]+$")
     private String email;
 
 }
