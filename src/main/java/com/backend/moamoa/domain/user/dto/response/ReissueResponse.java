3a8b3062e8b96f8d6172a4b7d6b02523a95b7a51
minu kim <kmw106933@naver.com>
2022-04-16 21:58:46 +0900

    [Feat] 토큰 재발급 정보 추가

---
+++ src/main/java/com/backend/moamoa/domain/user/dto/response/ReissueResponse.java
@@ -0,0 +1,36 @@
+package com.backend.moamoa.domain.user.dto.response;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel("토큰 재발급 반환 정보")
+public class ReissueResponse {
+
+    @ApiModelProperty(value = "accessToken의 헤더타입", example = "Bearer")
+    private String grantType;
+
+    @ApiModelProperty(value = "재발급된 accessToken")
+    private String accessToken;
+
+    @ApiModelProperty(value = "accessToken의 만료 기간 ms(30분)", example = "1800000")
+    private Long accessTokenExpireDate;
+
+    @ApiModelProperty(value = "refreshToken의 만료 기간 ms(일주일)", example = "604800000")
+    private Long refreshTokenExpireDate;
+
+    public static ReissueResponse toReissueResponse(TokenResponse tokenResponse) {
+        return ReissueResponse.builder()
+                .grantType(tokenResponse.getGrantType())
+                .accessToken(tokenResponse.getAccessToken())
+                .accessTokenExpireDate(tokenResponse.getAccessTokenExpireDate())
+                .refreshTokenExpireDate(tokenResponse.getRefreshTokenExpireDate())
+                .build();
+    }
+
+}
