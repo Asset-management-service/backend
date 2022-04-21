ffc387d2ea12efdc28aa5dd253fe9228844a6b17
ehgns852 <ehgns5668@naver.com>
2022-04-22 06:59:23 +0900

    [Refactor] 회원가입 시 디폴트 카테고리 생성

--- src/main/java/com/backend/moamoa/domain/user/oauth/service/CustomOAuth2UserService.java
+++ src/main/java/com/backend/moamoa/domain/user/oauth/service/CustomOAuth2UserService.java
@@ -1,5 +1,8 @@
 package com.backend.moamoa.domain.user.oauth.service;
 
+import com.backend.moamoa.domain.asset.entity.AssetCategory;
+import com.backend.moamoa.domain.asset.entity.AssetCategoryType;
+import com.backend.moamoa.domain.asset.repository.AssetCategoryRepository;
 import com.backend.moamoa.domain.user.oauth.entity.CustomUserDetails;
 import com.backend.moamoa.domain.user.oauth.entity.enums.ProviderType;
 import com.backend.moamoa.domain.user.entity.User;
@@ -15,6 +18,8 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -23,6 +28,7 @@ import java.util.Optional;
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     private final UserRepository userRepository;
+    private final AssetCategoryRepository assetCategoryRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -54,7 +60,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     }
 
     private User createUser(OAuth2UserInfo userInfo, ProviderType providerType) {
-        return userRepository.saveAndFlush(
+        User user = userRepository.saveAndFlush(
                 User.builder()
                         .userId(userInfo.getId())
                         .nickname(userInfo.getName())
@@ -65,6 +71,32 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
                         .providerType(providerType)
                         .build()
         );
+        List<AssetCategory> defaultCategories = createDefaultCategories(user);
+        defaultCategories
+                .forEach(category -> assetCategoryRepository.saveAndFlush(category));
+
+        return user;
+    }
+    private List<AssetCategory> createDefaultCategories(User user) {
+        return Arrays.asList(AssetCategory.createCategory(AssetCategoryType.FIXED, "주거비", user),
+                AssetCategory.createCategory(AssetCategoryType.FIXED, "보험료", user),
+                AssetCategory.createCategory(AssetCategoryType.FIXED, "통신비", user),
+                AssetCategory.createCategory(AssetCategoryType.FIXED, "유료구독서비스", user),
+                AssetCategory.createCategory(AssetCategoryType.FIXED, "예적금", user),
+                AssetCategory.createCategory(AssetCategoryType.FIXED, "기타", user),
+                AssetCategory.createCategory(AssetCategoryType.REVENUE, "월급", user),
+                AssetCategory.createCategory(AssetCategoryType.REVENUE, "주식", user),
+                AssetCategory.createCategory(AssetCategoryType.REVENUE, "기타", user),
+                AssetCategory.createCategory(AssetCategoryType.VARIABLE, "교통비", user),
+                AssetCategory.createCategory(AssetCategoryType.VARIABLE, "식비", user),
+                AssetCategory.createCategory(AssetCategoryType.VARIABLE, "생활비", user),
+                AssetCategory.createCategory(AssetCategoryType.VARIABLE, "쇼핑", user),
+                AssetCategory.createCategory(AssetCategoryType.VARIABLE, "문화생활", user),
+                AssetCategory.createCategory(AssetCategoryType.VARIABLE, "경조사", user),
+                AssetCategory.createCategory(AssetCategoryType.VARIABLE, "기타", user),
+                AssetCategory.createCategory(AssetCategoryType.PAYMENT, "현금", user),
+                AssetCategory.createCategory(AssetCategoryType.PAYMENT, "신용카드", user),
+                AssetCategory.createCategory(AssetCategoryType.PAYMENT, "체크카드", user));
     }
 
     private User updateUser(User user, OAuth2UserInfo userInfo) {
