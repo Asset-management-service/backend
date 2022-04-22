d0470743c56adf0e34ae42f5fcf373acaf1b8b02
minu kim <kmw106933@naver.com>
2022-04-22 16:52:22 +0900

    [Feat] 유저로부터 비율 가져오는 쿼리 추가

--- src/main/java/com/backend/moamoa/domain/asset/repository/ExpenditureRatioRepository.java
+++ src/main/java/com/backend/moamoa/domain/asset/repository/ExpenditureRatioRepository.java
@@ -1,7 +1,13 @@
 package com.backend.moamoa.domain.asset.repository;
 
 import com.backend.moamoa.domain.asset.entity.ExpenditureRatio;
+import com.backend.moamoa.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ExpenditureRatioRepository extends JpaRepository<ExpenditureRatio, Long> {
+
+    Optional<ExpenditureRatio> findByUser(User user);
+
 }
