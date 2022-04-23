75608db563128cb29252f88ea59c2acbd92a7206
김민우 <kmw106933@naver.com>
2022-04-24 00:02:24 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,4 +21,4 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log &
+nohup java -jar $DEPLOY_JAR --spring.config.location=/home/ec2-user/action/application.yml >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log &
