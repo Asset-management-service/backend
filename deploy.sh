ae8072c3fb66287f13a8a98f69ec50b22a6c75ed
김민우 <kmw106933@naver.com>
2022-04-28 11:32:59 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,4 +19,4 @@ else
 fi
 
 echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH --spring.config.location=/home/ec2-user/action/application.yml > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $JAR_PATH --spring.config.location=/home/ec2-user/action/application.yml /dev/null 2> /dev/null < /dev/null &
