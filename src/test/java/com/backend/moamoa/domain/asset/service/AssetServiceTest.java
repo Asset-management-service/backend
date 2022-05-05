1d18c7cf69aa2bbe643eea850adae2ee8380193c
ehgns852 <ehgns5668@naver.com>
2022-05-05 10:30:29 +0900

    [Add] 가계부 예산 설정- 성공 및 예산 설정을 찾지 못한 경우 테스트 코드

--- src/test/java/com/backend/moamoa/domain/asset/service/AssetServiceTest.java
+++ src/test/java/com/backend/moamoa/domain/asset/service/AssetServiceTest.java
@@ -2,9 +2,12 @@ package com.backend.moamoa.domain.asset.service;
 
 import com.backend.moamoa.builder.UserBuilder;
 import com.backend.moamoa.domain.asset.dto.request.AssetCategoryRequest;
+import com.backend.moamoa.domain.asset.dto.request.BudgetRequest;
 import com.backend.moamoa.domain.asset.entity.AssetCategory;
 import com.backend.moamoa.domain.asset.entity.AssetCategoryType;
+import com.backend.moamoa.domain.asset.entity.Budget;
 import com.backend.moamoa.domain.asset.repository.AssetCategoryRepository;
+import com.backend.moamoa.domain.asset.repository.BudgetRepository;
 import com.backend.moamoa.domain.user.entity.User;
 import com.backend.moamoa.global.utils.UserUtil;
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +16,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.*;
@@ -26,12 +31,15 @@ class AssetServiceTest {
     @Mock
     private AssetCategoryRepository assetCategoryRepository;
 
+    @Mock
+    private BudgetRepository budgetRepository;
+
     @InjectMocks
     private AssetService assetService;
 
 
     @Test
-    @DisplayName("가계부 설정 카테고리 생성")
+    @DisplayName("가계부 설정 카테고리 생성 - 성공")
     void addCategory() {
         //given
         User user = UserBuilder.dummyUser();
@@ -43,9 +51,53 @@ class AssetServiceTest {
         Long categoryId = assetService.addCategory(new AssetCategoryRequest(AssetCategoryType.FIXED, "월급"));
 
         //then
-        assertThat(categoryId).isEqualTo(1L);
+        assertThat(categoryId).isEqualTo(assetCategory.getId());
         verify(userUtil).findCurrentUser();
         verify(assetCategoryRepository).save(any(AssetCategory.class));
     }
+
+    @Test
+    @DisplayName("가계부 예산 설정 - 성공")
+    void addBudget() {
+        //given
+        User user = UserBuilder.dummyUser();
+        Budget budget = Budget.builder().id(1L).budgetAmount(1000000).user(user).build();
+
+        given(userUtil.findCurrentUser()).willReturn(user);
+        given(budgetRepository.findBudgetAmountByUser(any(User.class))).willReturn(Optional.of(budget));
+
+        //when
+        Long budgetId = assetService.addBudget(new BudgetRequest(100000));
+
+        //then
+        assertThat(budgetId).isEqualTo(budget.getId());
+        verify(userUtil).findCurrentUser();
+        verify(budgetRepository).findBudgetAmountByUser(any(User.class));
+    }
+
+    @Test
+    @DisplayName("가계부 예산 설정 - 예산 금액을 찾지 못한 경우 새로 만들어서 반환")
+    void addBudgetIsEmpty() {
+        //given
+        User user = UserBuilder.dummyUser();
+        Budget budget = Budget.builder().id(1L).budgetAmount(1000000).user(user).build();
+
+        given(userUtil.findCurrentUser()).willReturn(user);
+        given(budgetRepository.findBudgetAmountByUser(any(User.class)))
+                .willReturn(Optional.empty());
+        given(budgetRepository.save(any(Budget.class))).willReturn(budget);
+
+        //when
+        Long budgetId = assetService.addBudget(new BudgetRequest(100000));
+
+        //then
+        assertThat(budgetId).isEqualTo(budget.getId());
+
+        verify(userUtil).findCurrentUser();
+        verify(budgetRepository).findBudgetAmountByUser(any(User.class));
+        verify(budgetRepository).save(any(Budget.class));
+
+    }
+
 
 }
