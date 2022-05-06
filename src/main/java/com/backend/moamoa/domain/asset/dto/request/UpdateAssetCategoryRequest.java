01b99da8b94591f330867646e4800b0125a3f020
ehgns852 <ehgns5668@naver.com>
2022-05-07 00:27:14 +0900

    [Add] 가계부 설정 - 카테고리 수정 API 스웨거 코드 추가

--- src/main/java/com/backend/moamoa/domain/asset/dto/request/UpdateAssetCategoryRequest.java
+++ src/main/java/com/backend/moamoa/domain/asset/dto/request/UpdateAssetCategoryRequest.java
@@ -2,6 +2,7 @@ package com.backend.moamoa.domain.asset.dto.request;
 
 import com.backend.moamoa.domain.asset.entity.AssetCategoryType;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,12 +18,15 @@ import javax.validation.constraints.NotNull;
 public class UpdateAssetCategoryRequest {
 
     @NotNull
+    @ApiModelProperty(value = "카테고리 PK", example = "1", required = true)
     private Long categoryId;
 
     @NotNull
+    @ApiModelProperty(value = "카테고리 타입", example = "VARIABLE", required = true)
     private AssetCategoryType categoryType;
 
     @NotNull
+    @ApiModelProperty(value = "카테고리 이름", example = "월급", required = true)
     private String categoryName;
 
 }
