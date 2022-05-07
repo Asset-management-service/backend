a498aa7d389e0b08277dde23d79dc48f2f54ea90
ehgns852 <ehgns5668@naver.com>
2022-05-07 18:17:16 +0900

    [Add] 카테고리별 최근 게시글 조회 - 성공 테스트 코드

--- src/test/java/com/backend/moamoa/domain/post/controller/PostControllerTest.java
+++ src/test/java/com/backend/moamoa/domain/post/controller/PostControllerTest.java
@@ -2,6 +2,7 @@ package com.backend.moamoa.domain.post.controller;
 
 import com.backend.moamoa.domain.asset.controller.AssetController;
 import com.backend.moamoa.domain.post.dto.response.PostOneResponse;
+import com.backend.moamoa.domain.post.dto.response.RecentPostResponse;
 import com.backend.moamoa.domain.post.service.PostService;
 import com.backend.moamoa.domain.user.oauth.filter.JwtFilter;
 import com.backend.moamoa.global.bean.security.SecurityConfig;
@@ -17,6 +18,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -25,6 +29,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.result.StatusResultMatchers;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -83,6 +88,27 @@ class PostControllerTest {
         result.andExpect(status().isNotFound());
 
         verify(postService, times(1)).getOnePost(anyLong());
+    }
+
+    @Test
+    @DisplayName("카테고리별 최근 게시글 조회")
+    void getAllPosts() throws Exception {
+        //given
+        List<RecentPostResponse> recentPostResponses = List.of(
+                new RecentPostResponse(1L, "자유게시판", "모아모아", "test", 5, "ehgns5668", LocalDateTime.now(), 60),
+                new RecentPostResponse(2L, "자유게시판", "test1", "test1", 10, "ehgns", LocalDateTime.now(), 61));
+
+        PageImpl<RecentPostResponse> response = new PageImpl<>(recentPostResponses, PageRequest.of(1, 3), 2);
+
+        given(postService.getRecentPost(any(Pageable.class), anyString())).willReturn(response);
+
+        //when
+        ResultActions result = mockMvc.perform(get("/posts/recent?categoryName=자유게시판"));
+
+        //then
+        result.andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(response)))
+                .andDo(print());
     }
 
 }
