c9d0d461cb538645967ea227299f64f08d1a31b4
minu kim <kmw106933@naver.com>
2022-08-21 14:58:21 +0900

    [Refactor] 채팅 API 삭제

--- src/main/java/com/backend/moamoa/domain/chat/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.moamoa.domain.chat.repository;
-
-import com.backend.moamoa.domain.chat.entity.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
